d9a8fbae4b19ff5a9721df7290f1bb99b30b4068
Ryan Horoff <rhoroff@gmail.com>
2018-11-06 14:43:23 -0500

    Reimplements angular for student view

--- server/routes/authentication.routes.js
+++ server/routes/authentication.routes.js
@@ -1,20 +1,22 @@
 var path = require('path');
 module.exports = function(app, passport){
-    app.get('/', isLoggedIn, function(req, res, next){
-        if(req.user.role == "Professor"){
-            res.redirect('/professor');//TODO: Change to professor view
+
+    app.get('/', isLoggedIn, function(req, res, next){//Checks if user is logged in and then...
+        if(req.user.role == "Professor"){//Redirects based off of user roles
+            res.redirect('./professor.html');//TODO: Change to professor view
         }
         return next();
 
     });
     //=================
     //LOGIN
-    //=================
-    app.get('/login', function(req, res) {
-        res.render('login.ejs', {message: req.flash('loginMessage')});
+
+    //================= 
+    app.get('/login', function(req, res) {//Does login
+        res.render('login.ejs', {message: req.flash('loginMessage')});//Loads login view
     });
 
-    app.post('/login', passport.authenticate('local-login' , {
+    app.post('/login', passport.authenticate('local-login' , {//Uses passport to handle authentication
         successRedirect : '/',
         failureRedirect : '/login',
         failureFlash : true
@@ -29,7 +31,7 @@ module.exports = function(app, passport){
     //====================
     //SIGNUP
     //====================
-    app.get('/signup', function(req,res) {
+    app.get('/signup', function(req,res) {//redirects to singup
         res.render('signup.ejs', {message: req.flash('signupMessage')});
     });
 
@@ -51,7 +53,7 @@ module.exports = function(app, passport){
         res.redirect('/');
     });
 
-    function isLoggedIn(req,res,next) {
+    function isLoggedIn(req,res,next) {//USE THIS TO CHECK IF THERE IS A LOGIN
         if(req.isAuthenticated())
             return next();
 
