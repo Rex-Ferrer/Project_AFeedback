9bd643c9be830c764cec9c1bb2bc616511e0f3c3
Luis Garcia <garcialuis1998@yahoo.es>
2019-01-19 14:25:27 -0500

    Table MVP

--- listingsController.js
+++ listingsController.js
@@ -5,31 +5,41 @@ angular.module('listings').controller('ListingsController', ['$scope',
         {
             name: "Software Eng. II",
             totalApplicants: 5,
-            unreviewedApplicants: 2
+            unreviewedApplicants: 2,
+            params: ["Critical Thinking", "Leadership", "Teamwork"],
+            applications: [
+                {"rank": 2, "applicantName": "Luis", "Critical Thinking": 1, "Leadership": 2, "Teamwork": 3 },
+                {"rank": 1, "applicantName": "Rex", "Critical Thinking": 7, "Leadership": 2, "Teamwork": 5 }
+            ]
         },
         {
             name: "Software Eng. IV",
             totalApplicants: 15,
-            unreviewedApplicants: 1
+            unreviewedApplicants: 1,
+            params: ["Problem Solving", "Teamwork"],
+            applications: []
         }
     ];
 
+    // Data Tables
+    $(document).ready(function() {
+        $('#example').DataTable( {
+            columnDefs: [
+                {
+                    targets: [ 0, 1, 2 ],
+                    className: 'mdl-data-table__cell--non-numeric'
+                }
+            ]
+        } );
+    } );
 
-    /*
-      Implement these functions in the controller to make your application function
-      as described in the assignment spec.
-     */
-    $scope.addListing = function(code_, name_, latitude_,longitude_,address_) {
-      var obj = {
-        "code": code_,
-        "name": name_,
-        "coordinates": {
-          "latitude": latitude_,
-          "longitude": longitude_,
-        },
-        "address": address_
-      };
-      $scope.listings.push(obj);
+    $scope.toggleElement = function(element){
+        var toggleElement = element.children('.applicantTable');
+        if(toggleElement.style.display != "none"){
+            toggleElement.style.display = "none";
+        }else{
+            toggleElement.style.display = "block";
+        }
     };
     
   }
