d248d04a13663cad1210bc9081b3ffa3ad7d672e
Luis Garcia <garcialuis1998@yahoo.es>
2019-01-19 09:03:02 -0500

    Adding to table functionality.

---
+++ assets/js/tablemanager.js
@@ -0,0 +1,13 @@
+var companyName = "SwampHacks";
+var jobs = [
+    {
+        name: "Software Eng. II",
+        totalApplicants: 5,
+        unreviewedApplicants: 2
+    },
+    {
+        name: "Software Eng. IV",
+        totalApplicants: 15,
+        unreviewedApplicants: 1
+    }
+];
